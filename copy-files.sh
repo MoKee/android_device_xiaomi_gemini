5448a7a148f1698d639738d7d0a676c802fc9e70
crancocco <shulaibai@hustunique.com>
2016-05-02 23:06:53 +0800

    gemini:setup vendor

---
+++ copy-files.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+VENDOR=xiaomi
+DEVICE=gemini
+
+BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
+rm -rf $BASE/*
+
+for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$ | sed -e 's#^/system/##g'`; do
+    DIR=`dirname $FILE`
+    if [ ! -d $BASE/$DIR ]; then
+        mkdir -p $BASE/$DIR
+    fi
+    cp  /home/crancocco/libra/gemini/miui/system/$FILE $BASE/$FILE
+    #echo "Copy file $FILE done"
+done
+
+./setup-makefiles.sh
